1f0af5d1d3bd7f17d7b0524ef561418da89b88be
Jacob Koshy <jacob.koshy@nokia.com>
2017-03-17 14:35:59 +0530

    Shiro starts working

--- shiro-rest-auth/shiro-rest/src/main/java/com/karaf/rest/auth/impl/ShiroRestServiceImpl.java
+++ shiro-rest-auth/shiro-rest/src/main/java/com/karaf/rest/auth/impl/ShiroRestServiceImpl.java
@@ -2,10 +2,12 @@ package com.karaf.rest.auth.impl;
 
 import com.karaf.rest.auth.ShiroRestService;
 import com.karaf.shiro.auth.service.ShiroService;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
 
 /**
  * Created by vineethn on 3/15/2017.
  */
+@RequiresAuthentication
 public class ShiroRestServiceImpl implements ShiroRestService {
 
     private ShiroService shiroService;
--- shiro-rest-auth/shiro-service/src/main/java/com/karaf/shiro/auth/service/impl/ShiroServiceImpl.java
+++ shiro-rest-auth/shiro-service/src/main/java/com/karaf/shiro/auth/service/impl/ShiroServiceImpl.java
@@ -1,10 +1,12 @@
 package com.karaf.shiro.auth.service.impl;
 
 import com.karaf.shiro.auth.service.ShiroService;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
 
 /**
  * Created by vineethn on 3/15/2017.
  */
+@RequiresAuthentication
 public class ShiroServiceImpl implements ShiroService {
     @Override
     public String sayHello(String name) {
